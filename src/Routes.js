a144f56d6bab26a8acd15a75dd65652a71c07e66
Rahul Jain <rahul.jain@traveltriangle.com>
2020-07-15 18:10:40 +0530

    Updated build

--- src/Routes.js
+++ src/Routes.js
@@ -11,7 +11,7 @@ export default function Routes() {
   return (
     <Router>
       <Switch>
-        <Route exact path="/" component={App}>
+        <Route exact path="/calendar" component={App}>
         </Route>
       </Switch>
     </Router>
